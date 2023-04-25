e712457e0861b8d3b142c630e2c9b397501e1e7b
makblyon <michaelakpulu@gmail.com>
2023-04-25 14:46:52 -0700

    hope this works

--- utils.c
+++ utils.c
@@ -10,6 +10,7 @@ int is_printable(char c)
 {
 	if (c >= 32 && c < 127)
 		return (1);
+
 	return (0);
 }
 
@@ -46,6 +47,7 @@ int is_digit(char c)
 {
 	if (c >= '0' && c <= '9')
 		return (1);
+
 	return (0);
 }
 
@@ -56,7 +58,7 @@ int is_digit(char c)
  *
  * Return: Casted value of num
  */
-ilong int convert_size_number(long int num, int size)
+long int convert_size_number(long int num, int size)
 {
 	if (size == S_LONG)
 		return (num);
